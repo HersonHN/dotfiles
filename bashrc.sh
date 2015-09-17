81d2e98397ca399586722bbb3c334c9f9d9adeff
Herson Salinas <hersonhn@gmail.com>
2015-09-17 08:41:20 -0600

    prompt now in another file

--- bashrc.sh
+++ bashrc.sh
@@ -1,13 +1,10 @@
-### UTF-8 support ###
-
+### UTF-8 support
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
 
 
-
-### coloring ###
-
+### coloring
 export CLICOLOR=1
 export LSCOLORS=Gxfxcxdxbxegedabagacad
 
@@ -42,49 +39,9 @@ fi
 export PATH="$PATH:~/dotfiles/bin"
 
 
-
-### Prompt
-color_clear=\\[\\e[0m\\]
-color_bold=\\[\\e[1m\\]
-
-# Colors
-color_black=\\[\\e[30m\\]
-color_red=\\[\\e[31m\\]
-color_green=\\[\\e[32m\\]
-color_yellow=\\[\\e[33m\\]
-color_blue=\\[\\e[34m\\]
-color_magenta=\\[\\e[35m\\]
-color_cyan=\\[\\e[36m\\]
-color_white=\\[\\e[37m\\]
-
-
 ### Load the extras
 source ~/dotfiles/extras/marks.sh
-
-
-### Check if current dir is a valid Git repo ###
-gitb () {
-  git_branch=$(git branch | grep '*' | tr '* ' '\0')
-  echo $git_branch
-}
-
-check_git_prompt() {
-  local BRANCH="";
-
-  if [ -d .git ]; then
-    gitb &> /dev/null
-    BRANCH="$git_branch"
-  fi
-
-  if [ "$BRANCH" ]; then
-    BRANCH="$color_green""(""$git_branch"")""$color_red"
-  fi
-  
-  # Set the prompt
-  PS1="$color_bold""$color_red""\u""$BRANCH: ""\w"" > ""$color_clear"
-}
-
-PROMPT_COMMAND="check_git_prompt"
+source ~/dotfiles/extras/prompt.sh
 
 
 
---
+++ extras/prompt.sh
@@ -0,0 +1,41 @@
+
+### Prompt Colors
+color_clear=\\[\\e[0m\\]
+color_bold=\\[\\e[1m\\]
+color_black=\\[\\e[30m\\]
+color_red=\\[\\e[31m\\]
+color_green=\\[\\e[32m\\]
+color_yellow=\\[\\e[33m\\]
+color_blue=\\[\\e[34m\\]
+color_magenta=\\[\\e[35m\\]
+color_cyan=\\[\\e[36m\\]
+color_white=\\[\\e[37m\\]
+
+
+### Check if current dir is a valid Git repo
+gitb () {
+  git_branch=$(git branch | grep '*' | tr '* ' '\0')
+  echo $git_branch
+}
+
+
+check_git_prompt() {
+  local BRANCH="";
+
+  if [ -d .git ]; then
+    gitb &> /dev/null
+    BRANCH="$git_branch"
+  fi
+
+  if [ "$BRANCH" ]; then
+    BRANCH="$color_green""(""$git_branch"")""$color_red"
+  fi
+  
+  # Set the prompt
+  PS1="$color_bold""$color_red""\u""$BRANCH: ""\w"" > ""$color_clear"
+}
+
+
+
+PROMPT_COMMAND="check_git_prompt"
+
