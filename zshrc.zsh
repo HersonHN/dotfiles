fb47c6c6ec6c5900459f985234017523565cdebd
Herson Salinas <hersonhn@gmail.com>
2019-12-31 20:10:14 -0600

    configure history search for zsh

--- zshrc.zsh
+++ zshrc.zsh
@@ -1,1 +1,6 @@
 source ~/dotfiles/shell-config.sh
+
+## same as .inputrc, but for zsh
+bindkey "^[[A" history-beginning-search-backward
+bindkey "^[[B" history-beginning-search-forward
+setopt bash_auto_list
