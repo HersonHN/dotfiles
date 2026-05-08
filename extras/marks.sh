56f9d191a018dc00a8f901dd3f744051728b5876
Herson Salinas <hersonhn@gmail.com>
2026-05-08 15:54:39 -0600

    changes to marks.sh

--- extras/marks.sh
+++ extras/marks.sh
@@ -1,30 +1,36 @@
+#!/bin/bash
 
-# From the "Quickly navigate your filesystem from the command-line" post by Jeroen Janssens
+# Inspired by "Quickly navigate your filesystem from the command-line" post by Jeroen Janssens
 # http://jeroenjanssens.com/2013/08/16/quickly-navigate-your-filesystem-from-the-command-line.html
 
 export MARKPATH=$HOME/.marks
 
-function jump { 
-    cd -P "$MARKPATH/$1" 2>/dev/null || echo "No such mark: $1"
+function jump {
+    cd "$(cat "$MARKPATH/$1.mark")" 2>/dev/null || echo "No such mark: $1"
 }
-function mark { 
-    mkdir -p "$MARKPATH"; ln -s "$(pwd)" "$MARKPATH/$1"
-}
-function unmark { 
-    rm -i "$MARKPATH/$1"
-}
-function marks {
 
+function mark {
+    local MARK="$1"
+    local DIR="$(pwd)"
+    echo $DIR > "$MARKPATH/$MARK.mark"
+}
+
+function unmark { 
+    rm -i "$MARKPATH/$1.mark"
+}
+
+function marks {
     if [[ ! -d "$MARKPATH" ]]; then
         echo "There are no marks yet"
         return
     fi
 
-    if [[ `uname` == 'Darwin' ]]; then
-        ls -l "$MARKPATH" | tail -n +2 | sed 's/  / /g' | cut -d' ' -f9- | awk -F ' -> ' '{printf "%-10s -> %s\n", $1, $2}'
-    else
-        ls -l "$MARKPATH" | sed 's/  / /g' | cut -d' ' -f9- | sed 's/ -/\t-/g' && echo
-    fi
+    cd "$MARKPATH"
 
+    for file in *.mark; do
+    if [ -f "$file" ]; then
+        echo "${file/\.mark/}   ->   $(cat "$file")"
+    fi
+    done
 }
 
